5b22102e011706447ada489a8fc6aa8f0a92cae5
James Henry <james@henry.sc>
2021-11-26 14:08:53 +0000

    chore: add initial light and dark themes for storybook (#3)

---
+++ apps/storybook/.storybook/preview.js
@@ -0,0 +1,36 @@
+import { create } from '@storybook/theming';
+
+const themeBaseConfig = {
+  brandTitle: 'XRPL Components',
+  brandUrl: 'https://xrpl.org',
+  colorSecondary: '#7919FF',
+  // Taken from xrpl.org - "Work Sans" font is brought into Storybook from Google Fonts via preview-head.html
+  fontBase:
+    '"Work Sans",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+  fontCode: 'monospace',
+};
+
+const lightTheme = create({
+  ...themeBaseConfig,
+  base: 'light',
+  appBg: '#F5F5F7',
+  appContentBg: '#ffffff',
+  brandImage: 'https://xrpl.org/assets/img/XRPLedger_DevPortal-black.svg',
+});
+
+const darkTheme = create({
+  ...themeBaseConfig,
+  base: 'dark',
+  appBg: '#000000',
+  appContentBg: '#111112',
+  barBg: '#222222',
+  barTextColor: '#E0E0E1',
+  brandImage: 'https://xrpl.org/assets/img/XRPLedger_DevPortal-white.svg',
+});
+
+export const parameters = {
+  darkMode: {
+    dark: darkTheme,
+    light: lightTheme,
+  },
+};
