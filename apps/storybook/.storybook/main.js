9f0f2dce087ab59432ea7f383b1a5a6bd67a1d57
“JamesHenry” <james@henry.sc>
2021-11-26 10:55:22 +0000

    chore: add ci and storybook workflows

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -7,8 +7,8 @@ module.exports = {
 
   stories: [
     ...rootMain.stories,
-    '../../**/src/lib/**/*.stories.mdx',
-    '../../**/src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../libs/components/**/src/lib/**/*.stories.mdx',
+    '../../../libs/components/**/src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
   webpackFinal: async (config, { configType }) => {
--- apps/storybook/jest.config.js
+++ apps/storybook/jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/storybook',
+  coverageDirectory: '../../coverage/apps/storybook',
 };
