e415449009cc05f611d55a44afe2e48e20b3433e
“JamesHenry” <james@henry.sc>
2021-12-04 13:45:26 +0000

    chore: fix linting issue

--- libs/hooks/xrpl/src/use-xrpl-context.ts
+++ libs/hooks/xrpl/src/use-xrpl-context.ts
@@ -1,6 +1,7 @@
 import { createContext, useContext } from 'react';
 import { IUseXRPL } from './use-xrpl';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface XRPLContextProps extends IUseXRPL {}
 
 export const XRPLContext = createContext({} as XRPLContextProps);
