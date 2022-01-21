86e520e56320a7e461c431f378d7d3619cb7677d
“JamesHenry” <james@henry.sc>
2022-01-21 14:48:39 +0400

    chore: update workspace to latest deps

--- apps/storybook-e2e/src/integration/app.spec.ts
+++ apps/storybook-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('xrpl-components-demo', () => {
+describe('storybook', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('xrpl-components-demo', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome xrpl-components-demo');
+    getGreeting().contains('Welcome storybook');
   });
 });
--- apps/xrpl-components-demo/pages/index.tsx
+++ apps/xrpl-components-demo/pages/index.tsx
@@ -1,12 +1,7 @@
-import { AccountBalance } from '@xrpl-components/account-balance';
+import { AccountBalance } from '@xrpl-components/components/account-balance';
 import styles from './index.module.css';
 
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
   return (
     <div className={styles.page}>
       <AccountBalance account="rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn"></AccountBalance>
