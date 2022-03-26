eb1c70cf46eae1e0018844a0ed67e986257e0e61
James Henry <james@henry.sc>
2022-03-26 13:55:00 +0400

    chore: add website (#15)

---
+++ apps/website/next.config.js
@@ -0,0 +1,37 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+});
+
+module.exports = withNextra({
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  redirects: () => {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/getting-started',
+        statusCode: 301,
+      },
+      {
+        source: '/changelog',
+        destination: '/docs/changelog',
+        statusCode: 301,
+      },
+      {
+        source: '/change-log',
+        destination: '/docs/changelog',
+        statusCode: 301,
+      },
+      {
+        source: '/docs',
+        destination: '/docs/getting-started',
+        statusCode: 302,
+      },
+    ];
+  },
+});
---
+++ apps/website/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './components/**/*.tsx',
+    './pages/**/*.md',
+    './pages/**/*.mdx',
+    './pages/**/*.tsx',
+    './theme.config.js',
+    './styles.css',
+  ],
+};
